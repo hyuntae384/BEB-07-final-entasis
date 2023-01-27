0d7c704e751795d0fa595c5c63e96a6617eacae4
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-01-27 21:49:24 +0900

    company, token 디비 관련로직 구현

--- server/src/controllers/tokenController.js
+++ server/src/controllers/tokenController.js
@@ -2,4 +2,53 @@ const { users, companys, dividend_his, position_his, price_his } = require('../m
 const { depositFaucet, sendEtherToUser } = require('../chainUtils/etherUtils');
 const { getTokenBalance, getTokenName, signAndSendTx, sendTokenToUser } = require('../chainUtils/tokenUtils');
 
-module.exports = {}
+module.exports = {
+    buy: async(req,res,next)=> {
+        const {name,price,amount,wallet} = req.body;
+        // 조금 개선하면 불필요한 쿼리라고 생각 
+        const excompany = await companys.findOne({
+            where: { name },
+          });
+        try {
+            // 디비에서의 로직 컨트랙트는 따로 짜야함
+            if(!name || !excompany) return res.status(400).json({status : "fail"});
+            await position_his.create({
+                wallet: wallet,
+                order: 'buy',
+                price: price,
+                amount : amount,
+                // 수수료 계산
+                //fee: price * amount * 0.05
+                create_at: new Date()
+            });
+            return res.status(200).json({status : "success"});
+        } catch (err) {
+            console.err(err);
+            return next(err);
+        }
+    },
+    sell: async(req,res,next)=> {
+        const {name,price,amount,wallet} = req.body;
+        // 찾아보면 더 효율적인 방법이 있을것같음
+        const excompany = await companys.findOne({
+            where: { name },
+          });
+        try {
+            // 디비에서의 로직 컨트랙트는 따로 짜야함
+            if(!name || !excompany) return res.status(400).json({status : "fail"});
+            await position_his.create({
+                wallet: wallet,
+                order: 'sell',
+                price: price,
+                amount : amount,
+                // 수수료 계산
+                //fee: price * amount * 0.05
+                create_at: new Date()
+            });
+            return res.status(200).json({status : "success"});
+        } catch (err) {
+            console.err(err);
+            return next(err);
+        }
+    }
+}
