e5f1ebafd817bd72d40912f69d2b65cffe433a6d
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-01-30 10:56:47 +0900

    edit contract

--- contract/migrations/1_deploy_migration.js
+++ contract/migrations/1_deploy_migration.js
@@ -1,5 +1,5 @@
 const ERC1400 = artifacts.require('ERC1400');
 module.exports = async function(deployer) {
-  deployer.deploy(ERC1400, 'ENTAToken', 'ENTA', '0x17585470A862DAc925FEBDE93318B924cD2Fe4fe'); // 가나슈 재실행시 수정 필요!
+  deployer.deploy(ERC1400, 'ENTAToken', 'ENTA', '0xB01cBF8A6E692DA1EED2B7143cFBb9e8C4B164bf'); // 가나슈 재실행시 수정 필요!
   // const token = await ERC1400.deployed();
 }
