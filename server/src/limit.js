0ffcc4cf56f5045aee7235da112d1b8c7c13b36a
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2023-02-01 00:03:13 +0900

    배당금 구현

--- server/src/limit.js
+++ server/src/limit.js
@@ -3,7 +3,7 @@ const limit = require('express-rate-limit');
 
 exports.limiter = limit({
     windowMs: 60 * 1000,
-    max: 50,
+    max: 100,
     delayMs: 1000,
     handler(req,res) {
         res.status(this.statusCode).json({
