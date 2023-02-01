d60786308d3e330b8e0523cdfb5d07bc579899f2
bajnsk <baekjunseok63@gmail.com>
2023-02-01 23:02:18 +0900

    edit : basic buy

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,28 +1,79 @@
-import { useWeb3React } from "@web3-react/core";
+import {useWeb3React} from "@web3-react/core";
 import React from "react"
 import {useState, useEffect} from 'react';
-import {FaucetWallet, EnrollWallet, ChName, Tutorial, Score, Position, Account} from '../apis/user'
+import {FaucetWallet, EnrollWallet, ChName, Tutorial, Score, Position, Account} from '../apis/user';
+import {BuyToken, SellToken} from '../apis/token';
+import Web3 from "web3";
+import TokenABI from "../ABIs/ERC1400.json"
+
 const Order =()=>{
-    const [priceAmount, setPriceAmount] = useState({price : "", amount : ""});
+    const [amount, setAmount] = useState("");
+    const [price, setPrice] = useState("");
     const [isFaucet, setIsFaucet] = useState(false)
     const {chainId, account, active, activate, deactivate} = useWeb3React();
     const countNumber=(e)=>{
         return e.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,",")
     }
-
-    async function priceChange(e){
-        priceAmount.price = e.target.value;
-        setPriceAmount(priceAmount)
+    // 0xB681351a6F6018e8736e60aa46D1e18306d36A95
+    const web3 = new Web3(
+        window.ethereum || "http://18.182.9.156:8545"
+    );
+    const StABI = TokenABI.abi
+    const tokenContract = new web3.eth.Contract(StABI, '0x9464E6A0B91720B9a7f18e8A96ABeB01b72a139C');
+    
+    //console.log(userAccount);
+    // web3.eth.getAccounts().then(console.log);
+    const userAccount = useWeb3React().account;
+    function priceChange(e){
+        let curprice = e.target.value;
+        setPrice(curprice)
+        //console.log(price)
     }
 
-    async function amountChange(e){
-        priceAmount.amount = e.target.value;
-        setPriceAmount(priceAmount)
+    function amountChange(e){
+        let curamount = e.target.value;
+        setAmount(curamount)
+        console.log();
     }
 
-    async function ableChange(e){
-        
+    /* function SignTrans(){
+        const signTx = web3.eth.signTransaction({
+            from: userAccount,
+            gasPrice: "20000000000",
+            gas: "21000",
+            to: '0x9c3B07e4d0E97d08dF6EB4320687f8C64D0dacCB',
+            value: "10000000000000000"
+        }).then(console.log)
+
+        if (signTx.rawTransaction) {
+            web3.eth.sendSignedTransaction(signTx.rawTransaction);
+        }
+    } */
+
+    async function SendETH(){
+        const pubName = 'exchange';
+        console.log(pubName);
+        console.log(userAccount)
+        console.log(price);
+        console.log(amount);
+        const totalValue = amount * price * 1.0004;
+        console.log(totalValue);
+        web3.eth.sendTransaction({
+            from: userAccount,
+            to: '0x1289E91BB5Fd7F7769E367AAdEaD3Ee1a1829e69',
+            value: web3.utils.toWei(String(totalValue), 'ether')
+        });
+        BuyToken(pubName, String(price), String(amount), userAccount)
     }
+
+    /* async function SendETH(){
+        console.log(userAccount)
+         web3.eth.sendTransaction({
+            from: userAccount,
+            to: '0xD60e1416BE8657b8858443f2320D007672056eF5',
+            value: '1000000000000000000'
+        });
+    } */
     const ST_1 = {
         name:'BEBE',price:'200',amount:'20'
     };
@@ -59,6 +110,7 @@ const Order =()=>{
                 </button>
             </div>
         </form>
+        <button onClick={SendETH}><h5>HI</h5></button>
         <div className='assets'>
                             <h4>Assets</h4>
                             <div className='assets_wraper'>
