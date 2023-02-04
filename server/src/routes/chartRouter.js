45243a3750ceaea282347a0097ca9198f6748fa8
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-02-05 00:45:54 +0900

    거래제한 기능 수정

--- server/src/routes/chartRouter.js
+++ server/src/routes/chartRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const chartController = require('../controllers/chartController');
 
-router.get('/rtd', chartController.rtd);
+// router.get('/rtd', chartController.rtd);
 router.get('/total', chartController.total);
 router.post('/restrict', chartController.restrict);
 
