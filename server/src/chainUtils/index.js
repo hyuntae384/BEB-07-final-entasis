f0f986c5e66bc95d19d2af75623299d282569175
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-02-08 12:01:17 +0900

    가나슈 수정

--- server/src/chainUtils/index.js
+++ server/src/chainUtils/index.js
@@ -6,7 +6,7 @@ const { ENTA_CA, BEB_CA, LEO_CA } = process.env;
 const Web3 = require('web3');
 const TokenABI = require('./ABIs/ERC1400.json').abi; // 생성해야함
 
-const web3Endpoint = 'http://18.182.9.156:8545/';
+const web3Endpoint = 'http://13.231.123.183:8545/';
 
 const web3Http = new Web3(new Web3.providers.HttpProvider(web3Endpoint));
 
