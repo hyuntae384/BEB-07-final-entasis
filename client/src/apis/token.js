28b387aabbf384e893da2b66dc5822e81c3b1a4c
bajnsk <baekjunseok63@gmail.com>
2023-02-09 15:49:44 +0900

    edit : txHash api

--- client/src/apis/token.js
+++ client/src/apis/token.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 import { useState } from 'react';
 
 // Test API Request
-export const BuyToken = async(token,price,amount,wallet) => {
+export const BuyToken = async(token,price,amount,wallet,txin) => {
     if(wallet===null || wallet ===undefined)return new Error('Invalid Request!')
-    const resultBuyToken= await axios.post("http://localhost:5050/"+token+"/buy", {"price":price,"amount":amount,"wallet":wallet})
+    const resultBuyToken= await axios.post("http://localhost:5050/"+token+"/buy", {"price":price,"amount":amount,"wallet":wallet, "txin":txin})
     .then(res=>res)
     .then(err=>err)
     return resultBuyToken
 }
 
-export const SellToken = async(token,price,amount,wallet) => {
+export const SellToken = async(token,price,amount,wallet,txin) => {
     if(wallet===null || wallet ===undefined)return new Error('Invalid Request!')
-    const resultSellToken = await axios.post("http://localhost:5050/"+token+"/sell", {"price":price,"amount":amount,"wallet":wallet})
+    const resultSellToken = await axios.post("http://localhost:5050/"+token+"/sell", {"price":price,"amount":amount,"wallet":wallet, "txin":txin})
     .then(res=>res)
     .then(err=>err)
     return resultSellToken
