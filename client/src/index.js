0488f8ce5837dfdc4d984323b330f375a2714b57
Russ <yunsubin481@gmail.com>
2023-02-11 23:32:15 +0900

    change

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 // import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+// import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 
