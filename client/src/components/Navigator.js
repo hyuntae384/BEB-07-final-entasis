d9964b1fa033502412f4a1f4916ca564f98bad5c
Russ <yunsubin481@gmail.com>
2023-02-12 13:43:21 +0900

    Nav

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -7,14 +7,14 @@ import '../assets/css/main.css';
 import SelectBox from './Select';
 import Tutorials from "./Tutorials";
 
-const Navigator =({isCircuitBreaker,stName,setStName,companyPD,totalCurrentPrices,coorpName,circuitBreakerTimer,ST_Name,userModalIsOpen,setUserModalIsOpen,
+const Navigator =({isCircuitBreaker,stName,setStName,companyPD,totalCurrentPrices,coorpName,circuitBreakerTimer,ST_Name,userModalIsOpen,setUserModalIsOpen,date,currentPrice,pdModalIsOpen,setPdModalIsOpen
 
 })=>{
-    const [pdModalIsOpen, setPdModalIsOpen] = useState(false);
     const [tutorialsClicked,setTutorialsClicked] = useState(false)
-    let time = new Date()
-    let date = (59-time.getMinutes())%5+":"+(59-time.getSeconds());
-
+    const [currentPrices, setCurrentPrices] = useState({})
+    useEffect(()=>{
+        setCurrentPrices(currentPrice.totalCurrentPrices)
+    },[date])
     // const countNumber=(e)=>{
     //     return e.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,",")
     // }
@@ -55,7 +55,6 @@ const Navigator =({isCircuitBreaker,stName,setStName,companyPD,totalCurrentPrice
         divided:'10',
         next_ratio:'10',
     };;
-
 
     const PdModalOpen =()=>{
         document.body.style.overflow = 'hidden';
@@ -122,6 +121,9 @@ const Navigator =({isCircuitBreaker,stName,setStName,companyPD,totalCurrentPrice
             </div>
             </Modal>
             </div>
+            {/* <h5>{currentPrices.enta}</h5>
+            <h5>{currentPrices.beb}</h5>
+            <h5>{currentPrices.leo}</h5> */}
             {!isCircuitBreaker?
                 <div className="until_the_next_dividend_release">
                 <h4>Until the Next Dividend Release {date}</h4>
