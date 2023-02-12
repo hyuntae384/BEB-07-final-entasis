ed9e8fca31d1a96b84570dd8a98ff14949d0623f
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2023-02-12 15:25:12 +0900

    프론트 스테이킹 API 작성

--- client/src/apis/token.js
+++ client/src/apis/token.js
@@ -17,3 +17,19 @@ export const SellToken = async(token,price,amount,wallet,txin) => {
     .then(err=>err)
     return resultSellToken
 }
+
+export const Staking = async(token,price,amount,wallet,txin) => {
+    if(wallet===null || wallet ===undefined)return new Error('Invalid Request!')
+    const resultStaking = await axios.post("http://52.78.173.200:5050/"+token+"/staking", {"price":price,"amount":amount,"wallet":wallet, "txin":txin})
+    .then(res=>res)
+    .then(err=>err)
+    return resultStaking
+}
+
+export const Reward= async(token,price,amount,wallet,txout) => {
+    if(wallet===null || wallet ===undefined)return new Error('Invalid Request!')
+    const resultReward = await axios.post("http://52.78.173.200:5050/"+token+"/reward", {"price":price,"amount":amount,"wallet":wallet, "txout":txout})
+    .then(res=>res)
+    .then(err=>err)
+    return resultReward
+}
