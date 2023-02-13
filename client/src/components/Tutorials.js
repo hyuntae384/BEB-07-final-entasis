8a2b9a002ff728543da41b981634d8816fedfdd3
Russ <yunsubin481@gmail.com>
2023-02-13 22:44:52 +0900

    Tutorial

--- client/src/components/Tutorials.js
+++ client/src/components/Tutorials.js
@@ -18,7 +18,6 @@ const Tutorials =({account,faucetBtn,setUserModalIsOpen,setPdModalIsOpen,setCntH
     const [tutorialFinished, setTutorialFinished] = useState(true)
     const [tutorialScroll,setTutorialScroll]=useState(0)
     const [isFaucetModalOpen,setIsFaucetModalOpen]=useState(false);
-    {console.log(myPage)}
 
     const modalStyle = {
         overlay: {
